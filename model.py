de20fc79ca183eaad30897d3c686e4b716fafa64
Ate <poortinga.ate@gmail.com>
2021-02-04 21:32:09 +0700

    added mobilenetv3

--- model.py
+++ model.py
@@ -196,6 +196,35 @@ def build_model_mobilenet():
     model = Model(inputs, x)
     return model
 
+def build_mobileNetV3():
+    inputs = Input(shape=(None, None, 3), name="input_image")
+    
+    encoder = MobileNetV3Large(input_tensor=inputs, weights=None, include_top=False) #, alpha=0.35)
+
+    skip_connection_names = ["input_image", "re_lu_2", "re_lu_6", "re_lu_15"]
+    encoder_output = encoder.get_layer("re_lu_24").output
+    
+    f = [32, 64, 128, 256]
+    x = encoder_output
+    for i in range(1, len(skip_connection_names)+1, 1):
+        x_skip = encoder.get_layer(skip_connection_names[-i]).output
+        x = UpSampling2D((2, 2))(x)
+        x = Concatenate()([x, x_skip])
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+    x = Conv2D(2, (2, 2), padding="same")(x)
+    x = Activation("sigmoid")(x)
+    
+    model = Model(inputs, x)
+    return model
+
 def build_modelX(start_neurons=8):
     
     backbone = Xception(input_shape=(None, None,3), weights="imagenet", include_top=False)
